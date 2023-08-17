6ab0bd2e12f15c1f56bf7ea06290b079f8c27bad
Py Smith <kaluzhinas@gmail.com>
2023-08-17 23:05:53 +0700

    Add method get information about category system

--- src/api/v1/category.py
+++ src/api/v1/category.py
@@ -8,7 +8,7 @@ router = APIRouter()
 @router.get('/', description='name1')
 async def get_all_categories(request: Request,
                              db: SystemCategoryInfoService = Depends(get_categories)):
-    """Get information about all system"""
+    """Get information about all categories"""
     return await db.get_all_categories(request=request)
 
 
@@ -16,5 +16,5 @@ async def get_all_categories(request: Request,
 async def get_category_info_by_id(request: Request,
                                   category_id: int,
                                   db: SystemCategoryInfoService = Depends(get_categories)):
-    """Get information about system by id"""
+    """Get information about categories by id"""
     return await db.get_category_info_by_id(request=request, category_id=category_id)
